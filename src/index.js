f78f80283e74c19eacba1594b04ad281498bcbe2
llama-with-thumbs <vlad.filitovich@gmail.com>
2020-10-13 23:45:26 -0400

    feat: implement brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('')
+  let pairs = []
+  let current
+  let next
+
+  bracketsConfig.forEach(element => {
+    pairs.push(element.join(''))
+    })
+  for (let i = 0; i < arr.length; i++) {
+      current = arr[i]
+      next = arr[i+1]
+      if (pairs.includes(current+next)) {
+        arr.splice(i, 2)
+        i -= 2
+      }
+  }
+  if (arr.length === 0) {
+    return true
+  } else {
+    return false
+  }
 }
